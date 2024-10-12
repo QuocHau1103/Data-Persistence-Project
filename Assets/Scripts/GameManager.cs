b2b061dbe2ca985071fa69ce6ef77c9e8c982faa
QuocHau1103 <125619847+QuocHau1103@users.noreply.github.com>
2024-10-12 10:58:14 +0700

    initial commit
    starter breakout game imported

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public string playerName;
+    public string bestPlayerName;
+    public int bestScore = 0;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadNameAndBestScore();
+    }
+
+    public void SetPlayerName(string name)
+    {
+        playerName = name;
+    }
+
+    public string GetPlayerName()
+    {
+        return playerName;
+    }
+
+    public void SetBestScore(int score)
+    {
+        bestScore = score;
+    }
+
+    public int GetBestScore()
+    {
+        return bestScore;
+    }
+
+    public string GetBestPlayerName()
+    {
+        return bestPlayerName;
+    }
+
+    public void ResetBestScore()
+    {
+        bestScore = 0;
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public string playerName;
+        public int bestScore;
+        public string bestPlayerName;
+    }
+    public void SaveNameAndBestScore()
+    {
+        SaveData data = new SaveData();
+        data.playerName = playerName;
+        data.bestScore = bestScore;
+        data.bestPlayerName = bestPlayerName;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadNameAndBestScore()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            playerName = data.playerName;
+            bestScore = data.bestScore;
+            bestPlayerName = data.bestPlayerName;
+        }
+    }
+}
---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,36 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public TMP_InputField nameInputField;
+    public TMP_Text BestScoreText;
+
+    private void Start()
+    {
+        GameManager.Instance.LoadNameAndBestScore();
+        UpdateBestScoreText();
+    }
+
+    public void SubmitName()
+    {
+        GameManager.Instance.SetPlayerName(nameInputField.text);
+        Debug.Log("Player Name: " + GameManager.Instance.GetPlayerName());
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    private void UpdateBestScoreText()
+    {
+        BestScoreText.text = $"Best Score: {GameManager.Instance.GetBestPlayerName()} : {GameManager.Instance.GetBestScore()}";
+    }
+}
